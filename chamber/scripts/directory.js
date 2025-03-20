fb79dcaa3f8efda8ce893b8fbbff01530f0f4fac
jtheflipsmith <joshuasmith@byui.edu>
2025-03-20 11:30:29 -0600

    Mostly trying to ammend CSS

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -61,3 +61,11 @@ listButton.addEventListener("click", () => {
     cards.classList.add("list");
     cards.classList.remove("grid");
 });
+
+
+const year = new Date().getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+const modified = new Date().getDate();
+
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
