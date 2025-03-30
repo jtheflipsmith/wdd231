f9cd3476317f87d4eaeed039dbd18b4e027dc071
Joshua Smith <joshuasmith@byui.edu>
2025-03-29 21:11:01 -0600

    cosmetic changes

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -49,3 +49,12 @@ openButton4.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     dialogBox.close();
 });
+
+//  ------ FOOTER -----
+// --------------- FOOTER ----------------
+const year = new Date().getFullYear();
+document.querySelector("#currentYear").textContent = year;
+
+const modified = new Date().getDate();
+
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
