92c43ff17ef160c4e3d63ab4e6246823511f0e02
Joshua Smith <joshuasmith@byui.edu>
2025-03-29 20:39:35 -0600

    join page layout and function

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,9 +1,51 @@
 // Create variable to target button that will be pressed to open menu links
 const hamButton = document.querySelector("#hamButton");
 // Create variable to target menu links for manipulation.
-const navElement = document.querySelector(".mainLinks");
+const navElement = document.querySelector("#animateMe");
 
 // create an event listener for the button that will open and close menu links
 hamButton.addEventListener('click', () => {
-    navElement.classList.toggle("open")
-})
+    navElement.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+//Create dialog boxes for each membership level 
+const dialogBox = document.querySelector("#dialogBox");
+const openButton1 = document.querySelector("#membershipButton1");
+const openButton2 = document.querySelector('#membershipButton2');
+const openButton3 = document.querySelector('#membershipButton3');
+const openButton4 = document.querySelector('#membershipButton4');
+const closeButton = document.querySelector("#closeButton");
+const dialogText = document.querySelector("#dialogBox div");
+
+openButton1.addEventListener('click', () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = `
+    <h2>Non-Profit Membership</h2>
+    <p>Perks: Featured in the list and highlighted in a non-profit spotlight once weekly</p>`
+});
+
+openButton2.addEventListener('click', () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = `
+    <h2>Bronze Membership</h2>
+    <p>Perks: Added to directory list</p>`
+});
+
+openButton3.addEventListener('click', () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = `
+    <h2>Silver Membership</h2>
+    <p>Perks: Added to directiory and spotlighted weekly</p>`
+});
+
+openButton4.addEventListener('click', () => {
+    dialogBox.showModal();
+    dialogText.innerHTML = `
+    <h2>Gold Membership</h2>
+    <p>Perks: Added to directory and spotlighted at least once daily</p>`
+});
+
+closeButton.addEventListener('click', () => {
+    dialogBox.close();
+});
