1b39cbe362c068d52a1320f903c0a60ba7ad976f
Joshua Smith <joshuasmith@byui.edu>
2025-04-15 22:33:53 -0600

    javascript added for hamburger

--- health/scripts/nutrition.js
+++ health/scripts/nutrition.js
@@ -1,3 +1,15 @@
+// Create variable to target button that will be pressed to open menu links
+const hamButton = document.querySelector("#hamButton");
+// Create variable to target menu links for manipulation.
+const navElement = document.querySelector("#animateMe");
+
+// create an event listener for the button that will open and close menu links
+hamButton.addEventListener('click', () => {
+    navElement.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+
 // isolate data file into a variable
 const file = 'data/meals.json';
 // pull div class="cards" into a variable to manipulate in javascript
